b8d166245a750c2abdc0db70888717724dacd7fc
marciel_honorato <marciel.hsouza@hotmail.com>
2025-01-29 10:12:57 -0300

    exercicio02 adicionado

--- exercicios.py
+++ exercicios.py
@@ -2,19 +2,27 @@
 #Você está analisando um conjunto de dados de vendas e precisa garantir que todos os registros tenham valores positivos para quantidade e preço. 
 #Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos forem positivos ou "Dados inválidos" caso contrário.
 
-quantidade = -16
+    # quantidade = -16
 
-preco = -10.00
+    # preco = -10.00
 
-if quantidade > 0 and preco > 0:
-    print(f"Dados válidos")
-else: 
-    print(f"Dados inválidos")
+    # if quantidade > 0 and preco > 0:
+    #     print(f"Dados válidos")
+    # else: 
+    #     print(f"Dados inválidos")
  
 #Exercício 2: Classificação de Dados de Sensor
 #Imagine que você está trabalhando com dados de sensores IoT. Os dados incluem medições de temperatura. Você precisa classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'.
-#Considerando que:
+#Considerando que: Temperatura < 18°C é 'Baixa' | Temperatura >= 18°C e <= 26°C é 'Normal' | Temperatura > 26°C é 'Alta'
 
+temperatura = 21
+
+if temperatura < 18:
+    print(f"Temperatura Baixa")
+elif 18 <= temperatura <= 26:
+    print(f"Temperatura Normal")
+else:
+    print(f"Temperatura Alta")
 
 #Exercício 3: Filtragem de Logs por Severidade
 #Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'.Dado um registro de log em formato de dicionário 
