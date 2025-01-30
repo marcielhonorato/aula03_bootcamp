90660903cf0b2a32f680e7f35cad6a121bb13f5b
marciel_honorato <marciel.hsouza@hotmail.com>
2025-01-30 02:48:49 -0300

    exercicio04 adicionado

--- exercicios.py
+++ exercicios.py
@@ -28,15 +28,24 @@
 #Você está analisando logs de uma aplicação e precisa filtrar mensagens com severidade 'ERROR'.Dado um registro de log em formato de dicionário 
 #como log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}, escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 
-log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+    # log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
 
-if log['level'] == 'ERROR':
-    print(log['message'] )
+    # if log['level'] == 'ERROR':
+    #     print(log['message'] )
 
 #Exercício 4: Validação de Dados de Entrada
 #Antes de processar os dados de usuários em um sistema de recomendação, você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um email válido. 
 #Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
 
+idade = 15
+email = "marciel@gmail.com"
+
+if not 18 <= idade <= 65:
+    print("Idade fora da faixa etária")
+elif not '@' in email or not '.' in email:
+    print("Email Inválido")
+else:
+    print("Dados de usuário válidos")
 
 #Exercício 5: Detecção de Anomalias em Dados de Transações
 #Você está trabalhando em um sistema de detecção de fraude e precisa identificar transações suspeitas.Uma transação é considerada suspeita se o valor for superior a R$ 10.000 ou 
