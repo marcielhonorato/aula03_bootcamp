f57a9b98ef3c1f89923525225ffa7f3828a0022d
marciel_honorato <marciel.hsouza@hotmail.com>
2025-02-04 18:43:13 -0300

    exercicio13 adicionado

--- exercicios.py
+++ exercicios.py
@@ -141,9 +141,20 @@
 # 12. Validação de Entrada
 # Objetivo: Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
 
-numero = int(input("Digite um numero de 1 a 10: "))
+    # numero = int(input("Digite um numero de 1 a 10: "))
 
-while numero < 1 or numero > 10:
-    print(f"Número fora do intervalo")
-    numero = int(input("Digite um numero de 1 a 10:"))
-print("Número Válido!")
+    # while numero < 1 or numero > 10:
+    #     print(f"Número fora do intervalo")
+    #     numero = int(input("Digite um numero de 1 a 10:"))
+    # print("Número Válido!")
+
+# 13. Consumo de API Simulado
+# Objetivo: Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
+pagina_atual = 1
+paginas_totais = 5  # Simulação, na prática, isso viria da API
+
+while pagina_atual < paginas_totais:
+    print(f"Processando página {pagina_atual} de {paginas_totais}")
+    pagina_atual += 1
+print("Todas as páginas foram processadas.")
