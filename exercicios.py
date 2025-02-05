980bc7b2e32c26bfd2e70d6db607650658c51383
marciel_honorato <marciel.hsouza@hotmail.com>
2025-02-04 21:00:54 -0300

    exercicio14 adicionado

--- exercicios.py
+++ exercicios.py
@@ -151,10 +151,26 @@
 # 13. Consumo de API Simulado
 # Objetivo: Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
 
-pagina_atual = 1
-paginas_totais = 5  # Simulação, na prática, isso viria da API
+    # pagina_atual = 1
+    # paginas_totais = 5  # Simulação, na prática, isso viria da API
 
-while pagina_atual < paginas_totais:
-    print(f"Processando página {pagina_atual} de {paginas_totais}")
-    pagina_atual += 1
-print("Todas as páginas foram processadas.")
+    # while pagina_atual < paginas_totais:
+    #     print(f"Processando página {pagina_atual} de {paginas_totais}")
+    #     pagina_atual += 1
+    # print("Todas as páginas foram processadas.")
+
+# 14. Tentativas de Conexão
+# Objetivo: Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+tentativas_maximas = 5
+tentativa = 1
+
+while tentativa < tentativas_maximas:
+    print(f"Tentando de conectar {tentativa} de {tentativas_maximas} ...")
+    if False:
+        print("Conexão bem-sucedida!")
+        break
+    tentativa += 1
+else:
+    print("Falha ao conectar após várias tentativas.")
+    
